9fc6c532a7cb04fe41d4596541be9c6eaad2c61e
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-03-13 22:44:35 +0700

    remove GET allow http methods

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ type Contact struct {
 
 func allowCORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 }
 
